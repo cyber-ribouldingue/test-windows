fc46d16061c785696843a2b8fc4ab7ac2e179d8b
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 10:48:10 +0200

    Update setup.h

--- tests/qml-tests/setup.h
+++ tests/qml-tests/setup.h
@@ -2,18 +2,10 @@
 #define SETUP_H
 
 #include <QObject>
-#include <QQmlEngine>
 
 class Setup : public QObject
 {
     Q_OBJECT
-public:
-    Setup() = default;
-
-public slots:
-    void applicationAvailable();
-    void qmlEngineAvailable(QQmlEngine *engine);
-    void cleanupTestCase();
 };
 
 #endif // SETUP_H
