f355fdf8fed1182f681045a17894daf31917b397
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 08:20:27 +0200

    Create tst_test1.cpp

---
+++ tests/qt-tests/tst_test1.cpp
@@ -0,0 +1,16 @@
+#include <QtTest>
+
+class Test1 : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase() { }
+    void cleanupTestCase() { }
+    void test_case1() {
+        QVERIFY(true);
+    }
+};
+
+QTEST_MAIN(Test1)
+#include "tst_test1.moc"
