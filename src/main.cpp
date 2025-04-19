1ee5b54b90b0b48313cde83bee9b3c882e8aef41
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 10:42:19 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QUrl>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection
-    );
-    engine.loadFromModule("testapp", "Main");
+
+    // Version Qt5 : charger simplement main.qml
+    engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
