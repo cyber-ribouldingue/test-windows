b28cf684e20691da3d53f42e38a9e4fa87a940a2
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 11:04:33 +0200

    Update setup.h

--- tests/qml-tests/setup.h
+++ tests/qml-tests/setup.h
@@ -3,9 +3,16 @@
 
 #include <QObject>
 
+QT_FORWARD_DECLARE_CLASS(QQmlEngine)
+
 class Setup : public QObject
 {
     Q_OBJECT
+
+private slots:
+    void applicationAvailable();
+    void qmlEngineAvailable(QQmlEngine* engine);
+    void cleanupTestCase();
 };
 
 #endif // SETUP_H
