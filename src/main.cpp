fb1d481036015e835ecba9b7ee9d17c8d848f60a
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 10:46:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QQmlContext>
 #include <QUrl>
 
 int main(int argc, char *argv[])
@@ -8,8 +7,6 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-
-    // Version Qt5 : charger simplement main.qml
     engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
 
     if (engine.rootObjects().isEmpty())
