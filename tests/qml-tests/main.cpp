48319a3e65765d1b7c96fa7d8304b62992dc4424
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-20 20:42:53 +0200

    Update main.cpp

--- tests/qml-tests/main.cpp
+++ tests/qml-tests/main.cpp
@@ -1,4 +1,27 @@
-//test
 #include <QtQuickTest/quicktest.h>
+#include <QCoreApplication>
+#include <QFileInfo>
+#include <QDebug>
 
-QUICK_TEST_MAIN(QmlTests)
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    QString qmlFile;
+
+    // Si on détecte un environnement CI (comme GitHub Actions)
+    if (qEnvironmentVariableIsSet("CI") || qEnvironmentVariableIsSet("GITHUB_ACTIONS")) {
+        qmlFile = ":/TestHeadless.qml";
+    } else {
+        qmlFile = ":/Main.qml";
+    }
+
+    if (!QFileInfo::exists(qmlFile)) {
+        qCritical("QML file not found: %s", qUtf8Printable(qmlFile));
+        return 1;
+    }
+
+    qDebug() << "Loading QML file:" << qmlFile;
+
+    return quick_test_main(argc, argv, "QmlTests", qmlFile.toUtf8().constData());
+}
